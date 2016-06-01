3b4a8960326af124899a5d9d809a927507ead28d
DaCang <1061300519@qq.com>
2016-06-01 18:47:58 +0800

    A simple Spring MVC  demo based on maven

---
+++ msMVC01/src/main/java/com/songyl/webmvc/controller/helloController.java
@@ -0,0 +1,29 @@
+package com.songyl.webmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class helloController extends AbstractController {
+    @Override
+    public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+	    throws Exception {
+	String username = request.getParameter("username");
+	String userpsw = request.getParameter("userpsw");
+	String gender = request.getParameter("gender");
+	System.out.println("用户名：" + username);
+	System.out.println("密码：" + userpsw);
+	System.out.println("性别：" + gender);
+
+	//返回数据和页面
+	ModelAndView m_v = new ModelAndView("index");
+	m_v.addObject("username", username);
+	m_v.addObject("gender", gender);
+
+	return m_v;
+
+    }
+
+}
