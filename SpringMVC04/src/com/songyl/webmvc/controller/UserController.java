9e0cb3812d97e79bc8db4163f2d1d34e70ef4eb1
DaCang <1061300519@qq.com>
2016-06-03 11:18:26 +0800

    Update UserController.java

--- SpringMVC04/src/com/songyl/webmvc/controller/UserController.java
+++ SpringMVC04/src/com/songyl/webmvc/controller/UserController.java
@@ -9,8 +9,7 @@ import org.springframework.web.servlet.mvc.AbstractController;
 
 /**
  * @author Administrator
- * spring×¢½â
- * ¿ØÖÆÆ÷
+ * æ§åˆ¶å™¨
  *
  */
 @Controller
@@ -19,15 +18,15 @@ public class UserController extends AbstractController {
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse reponse)
 	    throws Exception {
-	System.out.println("+++++·ÃÎÊµ½ÁË#######");
+	System.out.println("+++++è®¿é—®åˆ°äº†#######");
 	String username = request.getParameter("username");
 	String userpsw = request.getParameter("userpsw");
 	String gender = request.getParameter("gender");
-	System.out.println("ÓÃ»§Ãû£º" + username);
-	System.out.println("ÃÜÂë£º" + userpsw);
-	System.out.println("ĞÔ±ğ£º" + gender);
+	System.out.println("ç”¨æˆ·åï¼š" + username);
+	System.out.println("å¯†ç ï¼š" + userpsw);
+	System.out.println("æ€§åˆ«ï¼š" + gender);
 
-	//·µ»ØÊı¾İºÍÒ³Ãæ
+	//è¿”å›æ•°æ®å’Œé¡µé¢
 	ModelAndView m_v = new ModelAndView("index");
 	m_v.addObject("username", username);
 	m_v.addObject("gender", gender);
